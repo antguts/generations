9222195c98d5f8c8a5d927801d55ee8edfc36b44
Anthony Gutilla <antgut035@gmail.com>
2016-04-29 15:41:28 -0400

    Update description and head.

--- app/mailers/message_mailer.rb
+++ app/mailers/message_mailer.rb
@@ -1,6 +1,6 @@
 class MessageMailer < ApplicationMailer
-  # default from: "no-reply@primarycode.io"
-  # Envelope From Address of <no-reply@primarycode.io> is not authorised - go to: https://control.authsmtp.com/from-add.php
+  # default from: "no-reply@generationshairsalon.io"
+  # Envelope From Address of <no-reply@generationshairsalon.io> is not authorised - go to: https://control.authsmtp.com/from-add.php
 
   def contact_us_message(message)
     @message = message
